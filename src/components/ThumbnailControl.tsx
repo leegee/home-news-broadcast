b59df9e6e6bc1bab03a4da7c354952dfcf471738
Lee Goddard <leegee@gmail.com>
2025-06-02 08:52:38 +0200

    Buttons to move thumbs

--- src/components/ThumbnailControl.tsx
+++ src/components/ThumbnailControl.tsx
@@ -9,13 +9,21 @@ type ThumbnailControlProps = {
 
 export default function ThumbnailControl(props: ThumbnailControlProps) {
     return (
-        <nav class={styles['thumbnail-control-component']}>
-            <li>
-                <button onClick={() => props.toSelect()} title='Play'> ▶ </button>
-            </li>
-            <li>
-                <button onClick={() => props.toDelete()} title='Clear'>🗑 </button>
-            </li>
-        </nav >
+        <>
+            <nav class={styles['thumbnail-control-component']}>
+                <li>
+                    <button onClick={() => props.onLeft()} title='Move left' class={styles['move-left']}> &lt; </button>
+                </li>
+                <li>
+                    <button onClick={() => props.toSelect()} title='Play'> ▶ </button>
+                </li>
+                <li>
+                    <button onClick={() => props.toDelete()} title='Remove'>🗑 </button>
+                </li>
+                <li>
+                    <button onClick={() => props.onRight()} title='Move right' class={styles['move-right']}> &gt; </button>
+                </li>
+            </nav>
+        </>
     );
 }
