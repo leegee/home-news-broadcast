ac43aa071d7b7c5a11277675a1175c6001568993
Lee Goddard <leegee@gmail.com>
2025-06-03 17:39:12 +0200

    Default

--- src/lib/stores/ui.ts
+++ src/lib/stores/ui.ts
@@ -18,8 +18,12 @@ export interface MediaSource {
     type: StreamType;
 }
 
+export const Default = {
+    banner: 'News From The Cat House'
+}
+
 export const [ticker, setTicker] = createSyncedPersistedSignal('cap-ticker', 'Click to edit');
-export const [banner, setBanner] = createSyncedPersistedSignal('cap-banner', 'Cat News');
+export const [banner, setBanner] = createSyncedPersistedSignal('cap-banner', Default.banner);
 export const [bannerImage, setBannerImage] = createSyncedPersistedSignal<string>('cap-banner-image', defatulCatImage);
 export const [qrCode, setQrCode] = createSyncedPersistedSignal<string>('cap-qr-code', '');
 export const [streamSource, setStreamSource] = createSyncedPersistedSignal<string | null>('cap-stream-source', null);
@@ -31,6 +35,7 @@ export const [isCapturing, setIsCapturing] = createSignal(false);
 
 
 export function initUiStorage() {
+    setBanner(Default.banner);
     setQrCode('');
     setStreamSource(STREAM_TYPES.NONE);
 }
