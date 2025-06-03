1367527057e7abeae8cdb6d13c4d042728da594b
Lee Goddard <leegee@gmail.com>
2025-06-03 11:29:00 +0200

    vars p2

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,3 +1,5 @@
 declare const __LOCAL_WEBRTC_ADDRESS__: string;
 declare const __LOCAL_IP__: string;
 declare const __RTC_PORT__: number;
+declare const __WS_IP__: string;
+declare const __WS_PORT__: number;
